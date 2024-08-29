036303b519a30c90978bfacddb4954d49154a4c9
Daniele Rapetti <iximiel@gmail.com>
2024-08-29 15:59:04 +0200

    setting up the inputs

--- install-plumed.sh
+++ install-plumed.sh
@@ -4,45 +4,47 @@
 set -e
 set -x
 
-suffix=""
-version=""
-repo=https://github.com/plumed/plumed2.git
-program_name=plumed
+cat <<EOF
+$SUFFIX
+$VERSION
+$REPO
+$PROGRAM_NAME
+EOF
 
-for opt; do
-    case "$opt" in
-    version=*) version="${opt#version=}" ;;
-    suffix=*)
-        suffix="--program-suffix=${opt#suffix=}"
-        program_name="plumed${opt#suffix=}"
-        ;;
-    repo=*) repo="${opt#repo=}" ;;
-    *)
-        echo "unknown option $opt"
-        exit 1
-        ;;
-    esac
-done
+# for opt; do
+#     case "$opt" in
+#     version=*) version="${opt#version=}" ;;
+#     suffix=*)
+#         suffix="--program-suffix=${opt#suffix=}"
+#         program_name="plumed${opt#suffix=}"
+#         ;;
+#     REPO=*) REPO="${opt#REPO=}" ;;
+#     *)
+#         echo "unknown option $opt"
+#         exit 1
+#         ;;
+#     esac
+# done
 
 cd "$(mktemp -dt plumed.XXXXXX)" || {
     echo "Failed to create tempdir"
     exit 1
 }
 
-git clone $repo
+git clone $REPO
 cd plumed2
 
-if [[ -n "$version" ]]; then
-    echo "installing plumed $version"
+if [[ -n "$VERSION" ]]; then
+    echo "installing plumed $VERSION"
 else
-    version=$(git tag --sort=version:refname |
+    VERSION=$(git tag --sort=version:refname |
         grep '^v2\.[0-9][0-9]*\.[0-9][0-9]*' |
         tail -n 1)
-    echo "installing latest stable plumed $version"
+    echo "installing latest stable plumed $VERSION"
 fi
 
-#cheking out to $version before compiling the dependency json for this $version
-git checkout $version
+#cheking out to $VERSION before compiling the dependency json for this $VERSION
+git checkout $VERSION
 
 if [[ $SETDEPENDENCIES ]]; then
     # This gets all the dependency information in plumed
@@ -75,27 +77,29 @@ if [[ $SETDEPENDENCIES ]]; then
             firstline=",\n"
         done
         echo -e '\n}'
-    } >"$GITHUB_WORKSPACE/_data/extradeps$version.json"
+    } >"$GITHUB_WORKSPACE/_data/extradeps$VERSION.json"
 fi
 hash=$(git rev-parse HEAD)
 
-if [[ -f $HOME/opt/lib/$program_name/$hash ]]; then
+if [[ -f $HOME/opt/lib/$PROGRAM_NAME/$hash ]]; then
     echo "ALREADY AVAILABLE, NO NEED TO REINSTALL"
 else
 
-    rm -fr "$HOME/opt/lib/$program_name"
-    rm -fr "$HOME/opt/bin/$program_name"
-    rm -fr "$HOME/opt/include/$program_name"
-    rm -fr "$HOME"/opt/lib/lib$program_name.so*
-
+    rm -fr "$HOME/opt/lib/$PROGRAM_NAME"
+    rm -fr "$HOME/opt/bin/$PROGRAM_NAME"
+    rm -fr "$HOME/opt/include/$PROGRAM_NAME"
+    rm -fr "$HOME"/opt/lib/lib$PROGRAM_NAME.so*
+    cat <<EOF
     ./configure --prefix="$HOME/opt" \
         --enable-modules=all \
         --enable-boost_serialization \
-        --enable-fftw $suffix \
+        --enable-fftw $SUFFIX \
         --enable-libtorch LDFLAGS=-Wl,-rpath,$LD_LIBRARY_PATH
+
+
     make -j 5
     make install
 
-    touch "$HOME/opt/lib/$program_name/$hash"
-
+    touch "$HOME/opt/lib/$PROGRAM_NAME/$hash"
+EOF
 fi
